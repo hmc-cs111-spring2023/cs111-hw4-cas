eabf85e55844333a9cba898a45c76651d38b3818
Cas Ruedy <89892181+Cruedy@users.noreply.github.com>
2023-02-15 03:58:53 +0000

    guessing game

--- src/main/scala/mastermind/Mastermind.scala
+++ src/main/scala/mastermind/Mastermind.scala
@@ -14,33 +14,26 @@ val validColors = List('B', 'Y', 'R', 'G')
 /** Get a random color from the list of valid colors */
 def getRandomColor(): Color =
   val rand = new scala.util.Random
-  val ind = rand.nextInt(validColors.length())
-  validColors.lift(ind)
+  val ind :Int = rand.nextInt(validColors.length)
+  validColors.apply(ind)
 
 
 /** Given four colors, make a board from them */
 def makeBoardFromColors(c1: Color, c2: Color, c3: Color, c4: Color): Board =
-  ???
-  /*
-  Board = c1.toString() + c2.toString() + c3.toString() + c4.toString()
-  Board*/
+  c1.toString() + c2.toString() + c3.toString() + c4.toString()
 
 /** Create a random board */
 def getRandomBoard(): Board =
-  ???
-  /*
   val c1 = getRandomColor()
   val c2 = getRandomColor()
   val c3 = getRandomColor()
   val c4 = getRandomColor()
-  makeBoardFromColors(c1, c2, c3, c4)*/
+  makeBoardFromColors(c1, c2, c3, c4)
 
 /** Play one round of the game */
 def playRound(board: Board): (Int, Int) =
-  ???
-  /*
-  correct_place = 0
-  correct_val = 0
+  var correct_place = 0
+  var correct_val = 0
   println("Enter a guess for spot 1:")
   val spot_one = readLine().toString()
   println("Enter a guess for spot 2:")
@@ -49,16 +42,18 @@ def playRound(board: Board): (Int, Int) =
   val spot_three = readLine().toString()
   println("Enter a guess for spot 4:")
   val spot_four = readLine().toString()
-  if Board.lift(1) == spot_one then correct_place += 1
-  else if Board contains spot_one then correct_val += 1
-  else if Board.lift(2) == spot_two then correct_place += 1
-  else if Board contains spot_two then correct_val += 1
-  else if Board.lift(3) == spot_three then correct_place += 1
-  else if Board contains spot_three then correct_val += 1
-  else if Board.lift(4) == spot_four then correct_place += 1
-  else if Board contains spot_four then correct_val += 1
-  println(correct_place.toString() + "color(s) are in the correct place.")
-  println(correct_val.toString() + "color(s) are correct but in the wrong place.")*/
+  val guess = spot_one + spot_two + spot_three + spot_four
+  scoreGuess(board, guess)
+  /*
+  if board.apply(1).toString() == spot_one then correct_place += 1
+  else if board contains spot_one then correct_val += 1
+  else if board.apply(2).toString() == spot_two then correct_place += 1
+  else if board contains spot_two then correct_val += 1
+  else if board.apply(3).toString() == spot_three then correct_place += 1
+  else if board contains spot_three then correct_val += 1
+  else if board.apply(4).toString() == spot_four then correct_place += 1
+  else if board contains spot_four then correct_val += 1 
+  (correct_place, correct_val)*/
 
 
 /** Score a guess
